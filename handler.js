1867383127dfe93647680af4fc16703472d930a6
hashmat2526 <hashmatajalali@gmail.com>
2019-04-04 02:38:34 -0700

    tests get method

--- handler.js
+++ handler.js
@@ -3,16 +3,50 @@
 const uuid = require('uuid');
 const AWS = require('aws-sdk');
 
+const dynamodb = new AWS.DynamoDB.DocumentClient({
+  // when we deploy this service , we have to remove these two lines
+  region: "localhost",
+  endpoint: "http://localhost:8000"
+});
 
-module.exports.hello = async (event) => {
-  return {
+module.exports.create = (event, context, callback) => {
+  // create a note and put it in the database
+  const response = {
     statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
+    body: JSON.stringify('Add a note')
   };
+  callback(null,response);
+}
+module.exports.getOne = (event, context, callback) => {
+  // create a note and put it in the database
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify('fetch a note')
+  };
+  callback(null,response);
+}
+module.exports.getAll = (event, context, callback) => {
+  // create a note and put it in the database
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify('fetch all notes')
+  };
+  callback(null,response);
+}
+module.exports.update = (event, context, callback) => {
+  // create a note and put it in the database
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify('Update a note')
+  };
+  callback(null,response);
+}
+module.exports.delete = (event, context, callback) => {
+  // create a note and put it in the database
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify('Delete a note')
+  };
+  callback(null,response);
+}
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
-};
